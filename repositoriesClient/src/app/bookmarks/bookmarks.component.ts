3e2f52c96eac80ddee6769501c3f922708f4aa30
sarafro <30698115+sarafro@users.noreply.github.com>
2023-02-28 10:51:30 +0200

    Delete unused oninit

--- repositoriesClient/src/app/bookmarks/bookmarks.component.ts
+++ repositoriesClient/src/app/bookmarks/bookmarks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Repository } from '../_models';
 import { RepositoryService } from '../_services';
@@ -8,14 +8,10 @@ import { RepositoryService } from '../_services';
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.scss']
 })
-export class BookmarksComponent implements OnInit {
+export class BookmarksComponent {
   BookmarksRepo$: Observable<Repository[]>;
 
   constructor(private repositoryService: RepositoryService) {
     this.BookmarksRepo$ = this.repositoryService.getBookmarksRepo()
   }
-
-  ngOnInit(): void {
-  }
-
 }
